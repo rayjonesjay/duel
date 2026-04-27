e72957d0588527f61e8d77e785a0da62c89b73aa
rayjonesjay <rayjaymuiruri@gmail.com>
2026-04-27 15:32:25 +0300

    change project name

--- src/duel/main.py
+++
@@ -1,2 +0,0 @@
-def main():
-    print("duel is a bitcoin library in python; dont use it am a noob programmer")
---
+++ src/duelcrypt/main.py
@@ -0,0 +1,2 @@
+def main():
+    print("duel crypt is a bitcoin library in python; dont use it am a noob programmer")
